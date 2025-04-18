386517fa26bd96e0134849aa7e3e7e93e6f79550
unknown <Josh Blake>
2025-04-18 17:31:38 -0400

    set static base in svelte & vite config. Reran build

---
+++ build/_app/immutable/entry/start.C-X3CWOL.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/IAGlaED5.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -2,19 +2,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-            // Default options:
-            pages: 'build', // The output directory name (default is 'build')
-            assets: 'build', // The output directory name (default is 'build')
-            fallback: 'index.html', // Use 'index.html' or '200.html' or '404.html' as fallback for SPAs
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
             precompress: false,
             strict: true
         }),
-	}
+        paths: {
+            base: '/static' 
+        }
+    }
 };
 
 export default config;
