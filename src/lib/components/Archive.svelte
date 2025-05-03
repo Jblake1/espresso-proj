23e8733ed6a991db3626ed4069a9b78e521a8fab
unknown <Josh Blake>
2025-05-03 01:43:26 -0400

    added flex-1 to summary text

--- src/lib/components/Archive.svelte
+++ src/lib/components/Archive.svelte
@@ -146,7 +146,8 @@
                                         {journey.drink}
                                     </p>
                                     <div class="h-3 border-r border-slate-300 flex-shrink-0"></div>
-                                    <p class="overflow-hidden text-ellipsis whitespace-nowrap bg-primary-500/80 px-2 py-0.5 rounded text-white text-sm w-auto max-w-full min-w-0">
+                                    {/* Use flex-1 to allow growing/shrinking, keep min-w-0 */}
+                                    <p class="overflow-hidden text-ellipsis whitespace-nowrap bg-primary-500/80 px-2 py-0.5 rounded text-white text-sm flex-1 min-w-0">
                                         {journey.coffeeBeans}
                                     </p>
                                 </div>
